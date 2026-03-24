b8e49e2b12ff43b6bc474cdb7e1b6911bcb943e0
Nikunj <nikunjskabra@gmail.com>
2026-03-24 14:36:01 +0530

    minor

--- disaster-management/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as Sidebar } from './Sidebar';
-export { default as DashboardLayout } from './DashboardLayout';
-export { ProtectedRoute } from './ProtectedRoute';
-export { default as StatusBadge } from './StatusBadge';
-export { default as ShiftCard } from './ShiftCard';
-export { default as DataTable } from './DataTable';
-export { default as EmergencyCard } from './EmergencyCard';
-export { default as DashboardCard } from './DashboardCard';
